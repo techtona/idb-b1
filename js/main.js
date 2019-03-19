431962d431a9344022d13c8d0c272d03fbba92af
Jati Pikukuh <jati.pikukuh@maiil.ugm.ac.id>
2019-03-19 10:46:20 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*
+* IndexedDB
+* */
+createDatabase();
+function createDatabase() {
+    if (!('indexedDB' in window)){
+        console.log('Web Browser tidak mendukung Indexed DB');
+        return;
+    }
+    var request = window.indexedDB.open('latihan-pwa',1);
+    request.onerror = errordbHandle;
+    request.onupgradeneeded = (e)=>{
+        var db = e.target.result;
+        db.onerror = errordbHandle;
+        var objectStore = db.createObjectStore('mahasiswa',
+            {keyPath: 'nim'});
+        console.log('Object store mahasiswa berhasil dibuat');
+    }
+    request.onsuccess = (e) => {
+        db = e.target.result;
+        db.error = errordbHandle;
+        console.log('Berhasil melakukan koneksi ke database lokal');
+        // lakukan sesuatu ...
+    }
+}
+
+function errordbHandle(e) {
+    console.log('Error DB : '+e.target.errorCode);
+}
